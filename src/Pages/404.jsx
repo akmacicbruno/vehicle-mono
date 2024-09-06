49277b3d18d8f951b5a7338dc934723a44c38edb
bruno <akmacicbruno@gmail.com>
2024-09-06 17:18:02 +0200

    404 styles added

--- src/Pages/404.jsx
+++ src/Pages/404.jsx
@@ -1,10 +1,18 @@
 import "../App.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div className="app">
-      <h1>Page not found.</h1>
+    <div className="App">
+      <div className="notfound-container">
+        <img src="404.svg" alt="not_found" width="450" height="450" />
+        <h1 className="notfound-title">Page not found</h1>
+        <p className="notfound-text">Accident occurred.</p>
+        <Link to="/" className="back-home-link">
+          <p>Go to safety</p>
+        </Link>
+      </div>
     </div>
   );
 }
