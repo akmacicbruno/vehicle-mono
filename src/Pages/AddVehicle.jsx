f891f9cb57474f7d9396e34262c391c81cfeeb4a
bruno <akmacicbruno@gmail.com>
2024-09-06 16:52:53 +0200

    Adding and editing vehicle forms modified, styles created

--- src/Pages/AddVehicle.jsx
+++ src/Pages/AddVehicle.jsx
@@ -3,55 +3,85 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 import { vehicleForm } from "../Stores/AddVehicleForm"; // Importirajte instancu forme
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/esm/Button";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
 const AddVehicleForm = observer(() => {
   return (
-    <div>
-      <Link to="/">Back home</Link>
-      <h1>Add New Vehicle</h1>
-      <form onSubmit={vehicleForm.handleSubmit}>
-        <div>
-          <label>{vehicleForm.$("vehicleMade").label}</label>
-          <input
-            type="text"
+    <div className="form-container">
+      <h1 className="form-title">Add New Vehicle</h1>
+      <form onSubmit={vehicleForm.handleSubmit} className="form">
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label={vehicleForm.$("vehicleMade").label}
+            className="mb-3"
             name="vehicleMade"
             value={vehicleForm.$("vehicleMade").value}
             onChange={(e) => vehicleForm.$("vehicleMade").onChange(e)}
-            placeholder={vehicleForm.$("vehicleMade").placeholder}
-          />
+          >
+            <Form.Control
+              type="text"
+              placeholder={vehicleForm.$("vehicleMade").placeholder}
+            />
+          </FloatingLabel>
         </div>
-        <div>
-          <label>{vehicleForm.$("origin").label}</label>
-          <input
-            type="text"
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label={vehicleForm.$("origin").label}
+            className="mb-3"
             name="origin"
             value={vehicleForm.$("origin").value}
             onChange={(e) => vehicleForm.$("origin").onChange(e)}
-            placeholder={vehicleForm.$("origin").placeholder}
-          />
+          >
+            <Form.Control
+              type="text"
+              placeholder={vehicleForm.$("origin").placeholder}
+            />
+          </FloatingLabel>
         </div>
-        <div>
-          <label>{vehicleForm.$("modelName").label}</label>
-          <input
-            type="text"
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label={vehicleForm.$("modelName").label}
+            className="mb-3"
             name="modelName"
             value={vehicleForm.$("modelName").value}
             onChange={(e) => vehicleForm.$("modelName").onChange(e)}
-            placeholder={vehicleForm.$("modelName").placeholder}
-          />
+          >
+            <Form.Control
+              type="text"
+              placeholder={vehicleForm.$("modelName").placeholder}
+            />
+          </FloatingLabel>
         </div>
-        <div>
-          <label>{vehicleForm.$("modelType").label}</label>
-          <input
-            type="text"
+
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label={vehicleForm.$("modelType").label}
+            className="mb-3"
             name="modelType"
             value={vehicleForm.$("modelType").value}
             onChange={(e) => vehicleForm.$("modelType").onChange(e)}
-            placeholder={vehicleForm.$("modelType").placeholder}
-          />
+          >
+            <Form.Control
+              type="text"
+              placeholder={vehicleForm.$("modelType").placeholder}
+            />
+          </FloatingLabel>
         </div>
 
-        <button type="submit">Submit</button>
+        <div className="form-buttons">
+          <Button variant="dark" type="submit">
+            Submit
+          </Button>
+          <Link to="/" className="form-cancel">
+            <p>Cancel</p>
+          </Link>
+        </div>
       </form>
     </div>
   );
--- src/Pages/UpdateVehicle.jsx
+++ src/Pages/UpdateVehicle.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react-lite";
 import { Link, useParams } from "react-router-dom";
+import FloatingLabel from "react-bootstrap/esm/FloatingLabel";
+import Form from "react-bootstrap/esm/Form";
+import Button from "react-bootstrap/esm/Button";
 
 const UpdateVehicle = observer(({ store }) => {
   store.vehicleId = useParams().id;
@@ -9,7 +12,78 @@ const UpdateVehicle = observer(({ store }) => {
   }, [store]);
 
   return (
-    <div>
+    <div className="form-container">
+      <h1 className="form-title">Edit vehicle</h1>
+      <form onSubmit={store.handleSubmit} className="form">
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Vehicle Made"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              value={store.$("vehicleMade").value}
+              onChange={(e) => store.$("vehicleMade").onChange(e)}
+              placeholder="Placeholder"
+            />
+          </FloatingLabel>
+        </div>
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Origin"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              value={store.$("origin").value}
+              placeholder="Placeholder"
+              onChange={(e) => store.$("origin").onChange(e)}
+            />
+          </FloatingLabel>
+        </div>
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Model"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              value={store.$("modelName").value}
+              placeholder="Placeholder"
+              onChange={(e) => store.$("modelName").onChange(e)}
+            />
+          </FloatingLabel>
+        </div>
+
+        <div className="form-input">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Type"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              value={store.$("modelType").value}
+              placeholder="Placeholder"
+              onChange={(e) => store.$("modelType").onChange(e)}
+            />
+          </FloatingLabel>
+        </div>
+
+        <div className="form-buttons">
+          <Button variant="dark" type="submit">
+            Submit
+          </Button>
+          <Link to="/" className="form-cancel">
+            <p>Cancel</p>
+          </Link>
+        </div>
+      </form>
+    </div>
+    /*<div>
       <Link to="/">Back home</Link>
       <h1>Update Vehicle</h1>
       <form onSubmit={store.handleSubmit}>
@@ -48,7 +122,7 @@ const UpdateVehicle = observer(({ store }) => {
 
         <button type="submit">Save</button>
       </form>
-    </div>
+    </div>*/
   );
 });
 
