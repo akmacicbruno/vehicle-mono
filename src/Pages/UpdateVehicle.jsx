7c0f9720dd456b411674367547194d917007707c
bruno <akmacicbruno@gmail.com>
2024-09-06 17:16:34 +0200

    Code purification

--- src/Pages/UpdateVehicle.jsx
+++ src/Pages/UpdateVehicle.jsx
@@ -83,46 +83,6 @@ const UpdateVehicle = observer(({ store }) => {
         </div>
       </form>
     </div>
-    /*<div>
-      <Link to="/">Back home</Link>
-      <h1>Update Vehicle</h1>
-      <form onSubmit={store.handleSubmit}>
-        <div>
-          <label>Vehicle Made:</label>
-          <input
-            type="text"
-            value={store.$("vehicleMade").value}
-            onChange={(e) => store.$("vehicleMade").onChange(e)}
-          />
-        </div>
-        <div>
-          <label>Origin:</label>
-          <input
-            type="text"
-            value={store.$("origin").value}
-            onChange={(e) => store.$("origin").onChange(e)}
-          />
-        </div>
-        <div>
-          <label>Model:</label>
-          <input
-            type="text"
-            value={store.$("modelName").value}
-            onChange={(e) => store.$("modelName").onChange(e)}
-          />
-        </div>
-        <div>
-          <label>Type:</label>
-          <input
-            type="text"
-            value={store.$("modelType").value}
-            onChange={(e) => store.$("modelType").onChange(e)}
-          />
-        </div>
-
-        <button type="submit">Save</button>
-      </form>
-    </div>*/
   );
 });
 
