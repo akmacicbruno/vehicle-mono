a986ba73df2dbcfd1c1475325ee24510a2600dff
bruno <akmacicbruno@gmail.com>
2024-09-06 16:50:42 +0200

    Navbar added, changes to layout

--- src/App.js
+++ src/App.js
@@ -7,18 +7,21 @@ import { updateVehicle } from "./Stores/UpdateVehicle";
 import AddVehicleForm from "./Pages/AddVehicle";
 import UpdateVehicle from "./Pages/UpdateVehicle";
 import NotFound from "./Pages/404";
+import Layout from "./Pages/Layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Vehicles store={vehiclesStore} />} />
-        <Route path="add" element={<AddVehicleForm />} />
-        <Route
-          path="update/:id"
-          element={<UpdateVehicle store={updateVehicle} />}
-        />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Vehicles store={vehiclesStore} />} />
+          <Route path="add" element={<AddVehicleForm />} />
+          <Route
+            path="update/:id"
+            element={<UpdateVehicle store={updateVehicle} />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Components/Navbar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/esm/Button";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand">
+            <img
+              src="car.svg"
+              alt="Logo"
+              width="64"
+              height="64"
+              className="d-inline-block align-text-top"
+            />
+            Vehicles
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse navbar-right"
+            id="navbarText"
+          >
+            <span>
+              <Link to="/add">
+                <Button variant="secondary">Add new vehicle</Button>
+              </Link>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
