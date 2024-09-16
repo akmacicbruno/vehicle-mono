4c9ed9a86c55452bb91b25068a265b521f5cd87b
bruno <akmacicbruno@gmail.com>
2024-09-16 15:06:55 +0200

    Minor changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="akmacicbruno.github.io/" element={<Layout />}>
+        <Route path="akmacicbruno.github.io/vehicle-mono/" element={<Layout />}>
           <Route index element={<Vehicles store={vehiclesStore} />} />
           <Route
             path="add"
